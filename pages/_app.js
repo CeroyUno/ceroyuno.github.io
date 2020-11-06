39c66e7c4ec2e7e6288fecd64091e77efed7045b
José J. Pérez Rivas <me@josejpr.com>
2020-11-06 21:49:56 +0100

    Include css file ui-kit general folder

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+// Import UI-KIT
+import '../ui-kit/style.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
