b7df611abdf8c46d401124f7a1879ba6372203e3
JoseJPR <me@josejpr.com>
2020-11-07 23:40:40 +0100

    error

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
       <main>
         <MenuComponent />
         <h1 className="title">
-          HOME | CeroyUno | Test rewrite Articles
+          HOME | CeroyUno | Error
         </h1>
         <div>
           { articles && <ArticlesComponent content={articles} /> }
